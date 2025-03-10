d373c44d21b718ebe44725a88a7c6b4a79049b7a
Harshil <prajapati_harshil@epam.com>
2025-03-11 02:23:55 +0530

    done

--- task11/jsrc/main/java/com/task11/handler/ApiHandler.java
+++ task11/jsrc/main/java/com/task11/handler/ApiHandler.java
@@ -3,18 +3,18 @@ package com.task11.handler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import java.util.Map;
-import syndicate.sdk.annotations.DependsOn;
-import syndicate.sdk.annotations.EnvironmentVariable;
-import syndicate.sdk.annotations.EnvironmentVariables;
-import syndicate.sdk.enums.ResourceType;
+//import syndicate.sdk.annotations.DependsOn;
+//import syndicate.sdk.annotations.EnvironmentVariable;
+//import syndicate.sdk.annotations.EnvironmentVariables;
+//import syndicate.sdk.enums.ResourceType;
 import com.task11.handler.ApiGatewayResponse;
 
-@DependsOn(resourceType = ResourceType.COGNITO_USER_POOL, name = "${booking_userpool}")
-@EnvironmentVariables(value = {
-		@EnvironmentVariable(key = "REGION", value = "${region}"),
-		@EnvironmentVariable(key = "COGNITO_ID", value = "${pool_name}", valueTransformer = USER_POOL_NAME_TO_USER_POOL_ID),
-		@EnvironmentVariable(key = "CLIENT_ID", value = "${pool_name}", valueTransformer = USER_POOL_NAME_TO_CLIENT_ID)
-})
+//@DependsOn(resourceType = ResourceType.COGNITO_USER_POOL, name = "${booking_userpool}")
+//@EnvironmentVariables(value = {
+//		@EnvironmentVariable(key = "REGION", value = "${region}"),
+//		@EnvironmentVariable(key = "COGNITO_ID", value = "${pool_name}", valueTransformer = USER_POOL_NAME_TO_USER_POOL_ID),
+//		@EnvironmentVariable(key = "CLIENT_ID", value = "${pool_name}", valueTransformer = USER_POOL_NAME_TO_CLIENT_ID)
+//})
 public class ApiHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
 	@Override
